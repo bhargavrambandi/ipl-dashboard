c60768aaf2d89cf0e13b11e7e1891d29818df45c
bhargavrambandi <bhargavram.jobs@gmail.com>
2024-09-01 02:50:40 +0530

    Sorted the Team names in home screen

--- src/main/java/com/bhargav/ipldashboard/service/TeamRepository.java
+++ src/main/java/com/bhargav/ipldashboard/service/TeamRepository.java
@@ -3,9 +3,14 @@ package com.bhargav.ipldashboard.service;
 import org.springframework.data.repository.CrudRepository;
 
 import com.bhargav.ipldashboard.model.Team;
+import java.util.List;
+
 
 public interface TeamRepository extends CrudRepository<Team, Long>  {
 
     Team findByTeamName(String teamName);
+    
+     List<Team> findAll();
+
     
 }
