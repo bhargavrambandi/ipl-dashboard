7e7329487bee7f14ae395a5e197795fa3bfed4f4
bhargavrambandi <bhargavram.jobs@gmail.com>
2024-09-01 21:36:35 +0530

    Refactored the code

--- src/main/java/com/bhargav/ipldashboard/service/TeamRepository.java
+++ src/main/java/com/bhargav/ipldashboard/service/TeamRepository.java
@@ -5,16 +5,10 @@ import org.springframework.data.repository.CrudRepository;
 import com.bhargav.ipldashboard.model.Team;
 import java.util.List;
 
+public interface TeamRepository extends CrudRepository<Team, Long> {
 
-public interface TeamRepository extends CrudRepository<Team, Long>  {
+	Team findByTeamName(String teamName);
 
-    Team findByTeamName(String teamName);
-    
-<<<<<<< Updated upstream
-     List<Team> findAll();
+	List<Team> findAll();
 
-    
-=======
-    List<Team> findAll();
->>>>>>> Stashed changes
 }
